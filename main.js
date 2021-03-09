4c9d0ee360b3f43601108974c9ab51f83e5b39bd
unknown <sihanwu@andrew.cmu.edu>
2021-03-08 21:31:32 -0500

    added complete list item function

--- main.js
+++ main.js
@@ -16,5 +16,16 @@ function deleteListItem(item) {
     item.parentNode.removeChild(item);
 }
 
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
+}
+
+
 
 
